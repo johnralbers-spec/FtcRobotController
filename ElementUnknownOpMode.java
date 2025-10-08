9a8378bc1ce61df070133868b119d226faad0dea
mindofsemaj-ux <mindofsemaj@gmail.com>
2025-10-07 19:15:57 -0500

    Add files via upload

--- ElementUnknownOpMode.java
+++ ElementUnknownOpMode.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
-@TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
+@TeleOp(name="OpMode", group="Linear OpMode")
 @Disabled
 public class ElementUnknownOpMode extends LinearOpMode {
     // Declare OpMode members for each of the 4 motors.
